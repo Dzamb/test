d727bcd938e6fedf664655cd866f5593b3642570
Kozaev Dzambolat <otrina1@yandex.ru>
2020-03-17 21:59:47 +0300

    внес некоторые изменения

--- sprite_tiled_map.py
+++ sprite_tiled_map.py
@@ -7,10 +7,12 @@ TILE_SCALING = 0.5
 PLAYER_SCALING = 1
 PLAYER_START_X = 196
 PLAYER_START_Y = 200
+PLAYER_MOVEMENT_SPEED = 7
+PLAYER_JUMP_SPEED = 30
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Sprite Tiled Map Example"
+SCREEN_TITLE = "17-th Update"
 SPRITE_PIXEL_SIZE = 128
 GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * TILE_SCALING)
 
@@ -102,7 +104,7 @@ class PlayerCharacter(arcade.Sprite):
             self.die_texture_pair.append(texture)
 
         #* Инициализируем начальную текстуру
-        self.texture = self.idle_texture_pair[0]
+        self.texture = self.idle_texture_pair[0][0]
 
         #? Что такое хит-боксы я пока не разобрался, но вроде нужно
         self.set_hit_box(self.texture.hit_box_points)
@@ -110,10 +112,22 @@ class PlayerCharacter(arcade.Sprite):
 
     def update_animation(self, delta_time=1 /60):
 
+
+#! ПРОБЛЕМА НАЧИНАЕТСЯ ЗДЕСЬ. НЕ ЗНАЮ КАК СОВМЕСТИТЬ КОД ДЛЯ СТАТИЧНОГО ПЕРСОНАЖА И АНИМИРОВАННОГО.
+
+        # #* анимация простоя(предыдущий вариант)
+        # if self.change_x == 0:
+        #     self.texture = self.idle_texture_pair[self.character_face_direction]
+        #     return
+
         #* анимация простоя
         if self.change_x == 0:
+            self.cur_texture += 1
+            if self.cur_texture >3:
+                self.cur_texture = 0
             self.texture = self.idle_texture_pair[self.character_face_direction]
             return
+
 
         #* анимация бега
         self.cur_texture += 1
@@ -122,8 +136,8 @@ class PlayerCharacter(arcade.Sprite):
         self.texture = self.run_texture_pair[self.set_texture][self.character_face_direction]
 
         #* анимация прыжка
-        self.cur_texture +=1
-        if self.cur_texture >3:
+        self.cur_texture += 1
+        if self.cur_texture > 3:
             self.cur_texture = 0
         self.texture = self.jump_texture_pair[self.cur_texture][self.character_face_direction]
 
@@ -256,6 +270,7 @@ class MyGame(arcade.Window):
         #TODO: self. view_score = 0
 
         #* Создаём спрайт листы
+        self.player_list = arcade.SpriteList()
         self.player_sprite = arcade.SpriteList()
         #TODO: self.coin_list = arcade.SpriteList()
 
@@ -321,121 +336,169 @@ class MyGame(arcade.Window):
         #TODO: self.coin_list.draw()
         self.player_list.draw()
 
+        #* Отрисовка очков на экране, прокрутка по области просмотра
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + view_bottom, arcade.scccolor.BLACK, 18)
 
 
+    def process_keychange(self):
+        #* Вызывается когда мы надимаем клавиши вверх/вниз или когда мы включаем/ выключаем лестницы
 
+        #* Процесс движения вверх/вниз
+        if self.up_pressed and not self.down_pressed:
+            if self.physics_engine.is_on_ladder():
+                self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+            elif self.physics_engine.can_jump() and not self.jump_need_reset:
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                self.jump_need_reset = True
+                #TODO: arcade.play_sound(self.jump_sound)
+        elif self.down_pressed and not self.up_pressed:
+            if self.physics_engine.is_on_ladder():
+                self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
 
-
-
-        #self.frame_count += 1
-
-        # This command has to happen before we start drawing
+        #* Процесс движения вверх/вниз когда мы на лестнице и не двигаемся
+        if self.physics_engine.is_on_ladder():
+            if not self.up_pressed and not self.down_pressed:
+                self.player_sprite.change_y = 0
+            elif self.up_pressed and self.down_pressed:
+                self.player_sprite.change_y = 0
         
+        #* Процесс движения влево/вправо
+        if self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        elif self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        else:
+            self.player_sprite.change_x = 0
 
-        scale = SCREEN_WIDTH / self.background.width
-        arcade.draw_lrwh_rectangle_textured(0, 0,
-                                            4800, 1280,
-                                            self.background)
-
-
-        # Draw all the sprites.
-        self.player_list.draw()
-        self.wall_list.draw()
-        self.coin_list.draw()
-
-        if self.last_time and self.frame_count % 60 == 0:
-            fps = 1.0 / (time.time() - self.last_time) * 60
-            self.fps_message = f"FPS: {fps:5.0f}"
-
-        if self.fps_message:
-            arcade.draw_text(self.fps_message, self.view_left + 10, self.view_bottom + 40, arcade.color.BLACK, 14)
-
-        if self.frame_count % 60 == 0:
-            self.last_time = time.time()
-
-        # Put the text on the screen.
-        # Adjust the text position based on the view port so that we don't
-        # scroll the text too.
-        distance = self.player.right
-        output = f"Distance: {distance}"
-        arcade.draw_text(output, self.view_left + 10, self.view_bottom + 20, arcade.color.BLACK, 14)
-
-        if self.game_over:
-            arcade.draw_text("Game Over", self.view_left + 200, self.view_bottom + 200, arcade.color.BLACK, 30)
 
     def on_key_press(self, key, modifiers):
-        """
-        Called whenever the mouse moves.
-        """
-        if key == arcade.key.UP:
-            if self.physics_engine.can_jump():
-                self.player.change_y = JUMP_SPEED
-        elif key == arcade.key.LEFT:
-            self.player.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player.change_x = MOVEMENT_SPEED
+        #* Вызывается когда мы клавиша нажата.
+        if key ==arcade.key.UP or key == arcade.key.W:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_pressed = True
+        
+        self.process_keychange()
+
 
     def on_key_release(self, key, modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player.change_x = 0
+        #* Вызывается когда пользователь отпускает клавишу.
+
+        if key ==arcade.key.UP or key == arcade.key.W:
+            self.up_pressed = False
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_pressed = False
+        
+        self.process_keychange()
+
 
     def on_update(self, delta_time):
-        """ Movement and game logic """
+        #* Процесс обновления спрайтов и игровой логики.
 
-        if self.player.right >= self.end_of_map:
-            self.game_over = True
+        #* Движение игрока с физическим движком
+        self.physics_engine.update()
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-        if not self.game_over:
-            self.physics_engine.update()
+        #* Обновление анимации
+        if self.physics_engine.can_jump():
+            self.player_sprite.can_jump = False
+        else:
+            self.player_sprite.can_jump = True
+        
+        #? Дальше мне немного не понятен процесс того что я описываю
+        if self.physics_engine.is_on_ladder() and not self.physics_engine.can_jump():
+            self.player_sprite.is_on_ladder = True
+            self.process_keychange()
+        else:
+            self.player_sprite.is_on_ladder = False
+            self.process_keychange()
 
-        coins_hit = arcade.check_for_collision_with_list(self.player, self.coin_list)
-        for coin in coins_hit:
+
+        #TODO: self.coin_list.update_animation(delta_time)
+        #TODO: self.background_list.update_animation(delta_time)
+        self.player_list.update_animation(delta_time)
+
+        #* Обновление преград с движущимися платформами
+        self.wall_list.update()
+
+        #* Логика отображающая не наткнулась ли движущаяся платформа
+        #* на преграду и не надо ли повернуть движение.
+        for wall in self.wall_list:
+
+            if wall.boundary_right and wall.right > wall.boundary_right and wall.change_x > 0:
+                wall.change_x *= -1
+            if wall.boundary_left and wall.left < wall.boundary_left and wall.change_y < 0:
+                wall.change_x *= -1
+            if wall.boundary_top and wall.top > wall.boundary_top and wall.change_y > 0:
+                wall.change_y *= -1
+            if wall.boundary_bottom and wall.bottom < wall.boundary_bottom and wall.change_y < 0:
+                wall.change_y *= -1
+
+        #* Отслеживание столкновений с монетками.
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        #* Цикл отслеживает со сколькими монетами мы столкнулись и удаляет их
+        for coin in coin_hit_list:
+
+            #* Выясняем сколько очков стоит монетка
+            if 'Points' not in coin.properties:
+                print("Внимание собранная монетка не имеет характеристику очков")
+            else:
+                points = int(coin.properties['Points'])
+                self.score += points
+
+            #* Удаление монетки.
             coin.remove_from_sprite_lists()
-            self.score += 1
+            arcade.play_sound(self.collect_coin_sound)
 
-        # --- Manage Scrolling ---
+            #* Отслеживание нужно ли нам поменять обзор(viewport)
+            changed_viewport = False
 
-        # Track if we need to change the view port
+            #* ===Скролинг===
 
-        changed = False
+            #* Скролинг влево
+            left_boundary = self.view_left + VIEWPORT_LEFT_MARGIN
+            if self.player_sprite.left < left_boundary:
+                self.view_left -= left_boundary - self.player_sprite.left
+                changed_viewport = True
 
-        # Scroll left
-        left_bndry = self.view_left + VIEWPORT_LEFT_MARGIN
-        if self.player.left < left_bndry:
-            self.view_left -= left_bndry - self.player.left
-            changed = True
+            #* Скролинг вправо
+            right_boundary = self.view_left + SCREEN_WIDTH - VIEWPORT_RIGHT_MARGIN
+            if self.player_sprite.right > right_boundary:
+                self.view_left += self.player_sprite.right - right_boundary
+                changed_viewport = True
 
-        # Scroll right
-        right_bndry = self.view_left + SCREEN_WIDTH - VIEWPORT_RIGHT_MARGIN
-        if self.player.right > right_bndry:
-            self.view_left += self.player.right - right_bndry
-            changed = True
+            #* Скролинг вверх
+            top_boundary = self.view_bottom + SCREEN_WIDTH - VIEWPORT_RIGHT_MARGIN
+            if self.player_sprite.top > top_boundary:
+                self.view_bottom += self.player_sprite.top - right_boundary
+                changed_viewport = True
 
-        # Scroll up
-        top_bndry = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN_TOP
-        if self.player.top > top_bndry:
-            self.view_bottom += self.player.top - top_bndry
-            changed = True
+            #* Скролинг вниз
+            bottom_boundary = self.view_bottom + VIEWPORT_RIGHT_MARGIN
+            if self.player_sprite.bottom < bottom_boundary:
+                self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+                changed_viewport = True
 
-        # Scroll down
-        bottom_bndry = self.view_bottom + VIEWPORT_MARGIN_BOTTOM
-        if self.player.bottom < bottom_bndry:
-            self.view_bottom -= bottom_bndry - self.player.bottom
-            changed = True
+            if changed_viewport:
+                #* Прокрутка только до целых чисел. Иначе мы получим пиксели 
+                #* которые не выстраиваются в линию на экране.
+                self.view_bottom = int(self.view_bottom)
+                self.view_left = int(self.view_left)
 
-        # If we need to scroll, go ahead and do it.
-        if changed:
-            self.view_left = int(self.view_left)
-            self.view_bottom = int(self.view_bottom)
-            arcade.set_viewport(self.view_left,
-                                SCREEN_WIDTH + self.view_left,
-                                self.view_bottom,
-                                SCREEN_HEIGHT + self.view_bottom)
+                #* Скролинг
+                arcade.set_viewport(self.view_left, 
+                                    SCREEN_WIDTH + self.view_left, 
+                                    self.view_bottom, 
+                                    SCREEN_HEIGHT + self.view_bottom)
 
 
 def main():
